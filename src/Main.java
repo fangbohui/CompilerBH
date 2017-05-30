7ecfafc195525bba0531514aaec82165a94a3c03
fangbohui <fangbohui@sjtu.edu.cn>
2017-05-31 03:28:09 +0800

    let me sleep

--- src/Main.java
+++ src/Main.java
@@ -23,10 +23,10 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 
 		int LOCAL;
-		LOCAL = 1;
+		LOCAL = 0;
 		InputStream is;
 		if (LOCAL == 1) {
-			is = new FileInputStream("/home/xzj/下载/fangbohui-compiler2017bh-56b699b20866/src/test.txt");
+			is = new FileInputStream("/home/xzj/Download/fangbohui-compiler2017bh-56b699b20866/src/test.txt");
 		} else {
 			is = new FileInputStream("/home/fangbohui/IdeaProjects/compiler2017bh/src/test.txt");
 		}
@@ -56,7 +56,7 @@ public class Main {
 		OutputStream os;
 
 		if (LOCAL == 1) {
-			os = new FileOutputStream("/home/xzj/下载/fangbohui-compiler2017bh-56b699b20866/src/fbh.asm");
+			os = new FileOutputStream("/home/xzj/Download/fangbohui-compiler2017bh-56b699b20866/src/fbh.asm");
 		} else {
 			os = new FileOutputStream("/home/fangbohui/IdeaProjects/compiler2017bh/src/test.txt");
 		}
